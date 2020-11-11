c2b70b9e0c4929e83999fc8ddb8813174f5c11a4
Irfan Khan <irfankhan501@outlook.com>
2020-11-11 13:03:31 +0530

    implamented the question

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,29 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    constructor(props) {
-		super(props);
-	};
-
-    render() {
-    	return(
-    		<div id="main">
-				{ /* Do not remove this main div!! */ }
-    		</div>
-    	);
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      para: "",
+    };
+  }
+  handleClick () {
+    this.setState({
+      para:
+        "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy",
+    });
+  }
+  render() {
+    return (
+      <div id="main">
+        <button id="click" onClick={this.handleClick.bind(this)}>
+          click me!
+        </button>
+        <p id="para">{this.state.para}</p>
+      </div>
+    );
+  }
 }
 
-
 export default App;
-
